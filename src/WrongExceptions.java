bcd094c406c229d34c7fdd95118db855c3531784
Artem <artempolzikov25@gmail.com>
2024-08-31 15:59:58 +0300

    hw is done

---
+++ src/WrongExceptions.java
@@ -0,0 +1,12 @@
+class WrongLoginException extends Exception {
+        public WrongLoginException(String message) {
+            super(message);
+        }
+    }
+
+    class WrongPasswordException extends Exception {
+        public WrongPasswordException(String message) {
+            super(message);
+        }
+    }
+
